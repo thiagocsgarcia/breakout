83b694b3645320d8125a81d4467c5ef29214008e
thiagocsgarcia <thiagocsgarcia@gmail.com>
2018-03-23 16:42:00 -0300

    novas features

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ env('URL_ADMIN_LOGIN') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="{{ route('admin.register') }}">Register</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,39 @@ use \Illuminate\Support\Facades\Gate;
 |
 */
 
+Route::Get('/user', function(){
+    \Illuminate\Support\Facades\Auth::LoginUsingId(2);
+});
+
 Route::get('/', function () {
-    //\Illuminate\Support\Facades\Auth::LoginUsingId(2);
-    if(Gate::allows('access-admin')){
+    //\Illuminate\Support\Facades\Auth::LoginUsingId(2);    
+    /*if(Gate::allows('access-admin')){
         return 'Usuário com permissão Admin';
     }else{
         return "Usuário sem permissão Admin";
-    }
-    //return view('welcome');
+    }*/
+
+    return view('welcome');
+});//->middleware('can:access-admin');
+
+
+    
+    
+
+Route::get('/home', function(){
+    return redirect()->route('admin.home');
 });
 
-Auth::routes();
+Route::group([
+    'prefix' => 'admin',     
+    'as' => 'admin.'
+], function(){
 
-Route::get('/home', 'HomeController@index')->name('home');
+    Auth::routes();
+
+    Route::group(['middleware' => 'can:access-admin'], function(){
+        Route::get('/home', 'HomeController@index')->name('home');
+    });
+
+    
+});
