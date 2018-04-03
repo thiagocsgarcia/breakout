515547782c4238ee1c89deea27b917e8d8e69ed1
Fabio Eduardo Bernardo Barros <fabiobarros@saovicente.local>
2018-04-03 12:27:23 -0300

    testando alteração no master, depois testarei abrir numa branch paralela

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading green">Pau no C# da Prefeitura</div>
+                <div class="panel-heading green">PNCPREF</div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ env('URL_ADMIN_LOGIN') }}">
